8fd948159312b02aee59a35cc129255a2b721e33
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-05-08 00:39:56 +1200

    init a razor front end

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2354a285f97eadaba858a40c044d36c284739378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b976cc68cae58724078c529bdde7caacd443bfa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
