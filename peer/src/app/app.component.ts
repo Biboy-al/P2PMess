3f37d4e156fcd4ec1674f78390500e7766b4761c
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-05-18 16:35:55 +1200

    Imported angular, and tailwind

--- peer/src/app/app.component.ts
+++ peer/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatSidenavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'peer';
