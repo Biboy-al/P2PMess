fea34687838dfebeecd067acddeccf9eac9c4202
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-05-20 01:26:33 +1200

    made changes to server

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd948159312b02aee59a35cc129255a2b721e33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ecf1c2c02acf6be08d993ea7fb7de25f0203bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
